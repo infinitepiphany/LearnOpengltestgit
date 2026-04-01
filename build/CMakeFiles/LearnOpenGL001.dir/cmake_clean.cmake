616281566af850e81dc5f51c2fce89db5025c1e7
Qixing Zhang <infinitedialectics@gmail.com>
2026-04-01 13:28:01 +0800

    first

---
+++ build/CMakeFiles/LearnOpenGL001.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LearnOpenGL001.dir/src/glad.c.o"
+  "CMakeFiles/LearnOpenGL001.dir/src/glad.c.o.d"
+  "CMakeFiles/LearnOpenGL001.dir/src/main.cpp.o"
+  "CMakeFiles/LearnOpenGL001.dir/src/main.cpp.o.d"
+  "LearnOpenGL001"
+  "LearnOpenGL001.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/LearnOpenGL001.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/LearnOpenGL001.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for LearnOpenGL001.
+# This may be replaced when dependencies are built.
